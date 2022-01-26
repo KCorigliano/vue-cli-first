e33cb8e959041bd87d6fcdb499bd736d85042043
KCorigliano <corigliano98@hotmail.com>
2022-01-26 16:10:12 +0100

    Jumbotron created

---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="jumbotron"></div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+  }
+}
+</script>
+<style>
+.title{
+    width: 800px;
+    height: 350px;
+    background-color: grey;
+    border: 3px solid black;
+    border-radius: 15px;
+    margin: 0 auto;
+}
+</style>
