4ee474dfd9348b46f722ce9decfb3c3a3e04e74f
KCorigliano <corigliano98@hotmail.com>
2022-01-26 16:03:59 +0100

    Title creation

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="title">
+    <h1>My first App with Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style>
+.title{
+    color: red;
+}
+</style>
